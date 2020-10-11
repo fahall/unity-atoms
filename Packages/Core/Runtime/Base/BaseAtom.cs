c5f546fa9b195283f2d69a63298bf9d12a3144e3
alex hall <alex@alexhall.org>
2020-10-11 16:37:27 -0700

    BaseAtom optionally inherits form Odin's SerializedScriptableObject for better generics support when using OdinInspector.

--- Packages/Core/Runtime/Base/BaseAtom.cs
+++ Packages/Core/Runtime/Base/BaseAtom.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
+#if ODIN_INSPECTOR && UNITY_ATOMS_USE_ODIN
+using Sirenix.OdinInspector;
+#endif
 
 namespace UnityAtoms
 {
     /// <summary>
     /// None generic base class for all Atoms.
     /// </summary>
+#if ODIN_INSPECTOR && UNITY_ATOMS_USE_ODIN
+    public abstract class BaseAtom : SerializedScriptableObject
+#else
     public abstract class BaseAtom : ScriptableObject
+#endif
     {
         /// <summary>
         /// A description of the Atom made for documentation purposes.
